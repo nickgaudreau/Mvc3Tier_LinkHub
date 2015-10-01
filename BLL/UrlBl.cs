99897ee977106da5207e23bdcc81aae79cc2a13f
nickgoodrow <nickgoodrow@gmail.com>
2015-09-30 21:01:32 -0400

    11: Creating Business Logic Layer And Implement BrowseUrLs BLL to cal DAL get all urls... next get only approved

---
+++ BLL/UrlBl.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BOL;
+using DAL;
+
+namespace BLL
+{
+    public class UrlBl
+    {
+        // pass the data from BLL to DAL 
+
+        private readonly UrlDbRepository objDb;
+
+        public UrlBl()
+        {
+            objDb = new UrlDbRepository();
+        }
+
+        public IEnumerable<tbl_Url> GetAll()
+        {
+            return objDb.GetAll();
+        }
+
+        public tbl_Url GetById(int id)
+        {
+            return objDb.GetById(id);
+        }
+
+        public void Insert(tbl_Url url)
+        {
+            objDb.Insert(url);
+        }
+
+        public void Delete(int id)
+        {
+           objDb.Delete(id);
+        }
+
+        public void Update(tbl_Url url)
+        {
+            objDb.Update(url);
+        }
+
+
+    }
+}
--- Mvc3Tier_LinkHub/Areas/Common/Controllers/BrowseUrlController.cs
+++ Mvc3Tier_LinkHub/Areas/Common/Controllers/BrowseUrlController.cs
@@ -1,18 +1,25 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
+using BLL;
 
 namespace Mvc3Tier_LinkHub.Areas.Common.Controllers
 {
     public class BrowseUrlController : Controller
     {
+
+        private UrlBl objBl = new UrlBl();
+
+        // Constructor instantiate every time we call Controller
+        public BrowseUrlController()
+        {
+            objBl = new UrlBl();
+        }
+
+
         // GET: Common/BrowseUrl
         public ActionResult Index()
         {
-
-            return View();
+            var urls = objBl.GetAll();
+            return View(urls);
         }
     }
 }
