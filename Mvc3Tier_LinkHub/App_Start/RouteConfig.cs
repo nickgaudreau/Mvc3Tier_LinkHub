7088f05cc40d0a981e610bf67b3e02562aa9e4a9
nickgoodrow <nickgoodrow@gmail.com>
2015-09-30 20:08:11 -0400

    9: Implementing Bootstrap: Setup the default area in route config….  Set _Layout links to proper area/controller/actions

--- Mvc3Tier_LinkHub/App_Start/RouteConfig.cs
+++ Mvc3Tier_LinkHub/App_Start/RouteConfig.cs
@@ -18,6 +18,10 @@ namespace Mvc3Tier_LinkHub
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
             );
+            // Force are default to be Common
+            var route = RouteTable.Routes[routes.Count - 1] as Route;
+            if (route != null)
+                route.DataTokens["area"] = "Common";
         }
     }
 }
