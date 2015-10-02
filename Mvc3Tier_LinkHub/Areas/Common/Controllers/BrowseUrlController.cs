4289fe7b282ad75f4e893aea8ccb1dcf7c5d2b1d
nickgoodrow <nickgoodrow@gmail.com>
2015-10-01 22:13:49 -0400

    14: Implementing Custom Paging in MVC on BrowseUrls table

--- Mvc3Tier_LinkHub/Areas/Common/Controllers/BrowseUrlController.cs
+++ Mvc3Tier_LinkHub/Areas/Common/Controllers/BrowseUrlController.cs
@@ -1,4 +1,5 @@
-﻿using System.Linq;
+﻿using System;
+using System.Linq;
 using System.Web.Mvc;
 using BLL;
 
@@ -17,7 +18,7 @@ namespace Mvc3Tier_LinkHub.Areas.Common.Controllers
 
 
         // GET: Common/BrowseUrl
-        public ActionResult Index(string sortOrder, string sortBy)
+        public ActionResult Index(string sortOrder, string sortBy, string page)
         {
             ViewBag.SortOrder = sortOrder; // we pass this so the opposite sorting is pass into the Query string when clicked again
             ViewBag.SortBy = sortBy;
@@ -87,6 +88,15 @@ namespace Mvc3Tier_LinkHub.Areas.Common.Controllers
 
             }
 
+            // Find page count. If 10 records per page and we have 37 records then it is 4 pages
+            ViewBag.TotalPages = Math.Ceiling(objBl.GetAll().Count(x => x.c_IsApproved == "A") /10.0 );
+
+            int currentPage = int.Parse(page == null ? "1" : page);
+
+            ViewBag.Page = currentPage; // to know which page to highlight even when we sort, and in the query string
+
+            // take the current page - 1 and mutli by 10 to give a start point in the data, then take next 10 records to display
+            urls = urls.Skip((currentPage - 1) * 10).Take(10);
 
             return View(urls);
         }
