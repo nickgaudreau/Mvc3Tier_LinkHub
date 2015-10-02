e3bd7ed1818a5206d99497db9218e88ec4f1e510
nickgoodrow <nickgoodrow@gmail.com>
2015-10-01 21:40:01 -0400

    13: Implementing Custom Sorting in MVC on BrowseUrls table on UrlTitle and other fields

--- Mvc3Tier_LinkHub/Areas/Common/Controllers/BrowseUrlController.cs
+++ Mvc3Tier_LinkHub/Areas/Common/Controllers/BrowseUrlController.cs
@@ -17,9 +17,77 @@ namespace Mvc3Tier_LinkHub.Areas.Common.Controllers
 
 
         // GET: Common/BrowseUrl
-        public ActionResult Index()
+        public ActionResult Index(string sortOrder, string sortBy)
         {
-            var urls = objBl.GetAll().Where(x => x.c_IsApproved == "A").ToList();
+            ViewBag.SortOrder = sortOrder; // we pass this so the opposite sorting is pass into the Query string when clicked again
+            ViewBag.SortBy = sortBy;
+            var urls = objBl.GetAll().Where(x => x.c_IsApproved == "A");
+
+            switch (sortBy)
+            {
+                case "Title":
+                    switch (sortOrder)
+                    {
+                        case "Asc":
+                            urls = urls.OrderBy(x => x.c_UrlTitle).ToList();
+                            break;
+                        case "Desc":
+                            urls = urls.OrderByDescending(x => x.c_UrlTitle).ToList();
+                            break;
+                        default:
+                            break;
+                    }
+                    break;
+
+                case "Url":
+                    switch (sortOrder)
+                    {
+                        case "Asc":
+                            urls = urls.OrderBy(x => x.c_Url).ToList();
+                            break;
+                        case "Desc":
+                            urls = urls.OrderByDescending(x => x.c_Url).ToList();
+                            break;
+                        default:
+                            break;
+                    }
+                    break;
+
+                case "Description":
+                    switch (sortOrder)
+                    {
+                        case "Asc":
+                            urls = urls.OrderBy(x => x.c_UrlDesc).ToList();
+                            break;
+                        case "Desc":
+                            urls = urls.OrderByDescending(x => x.c_UrlDesc).ToList();
+                            break;
+                        default:
+                            break;
+                    }
+                    break;
+
+                case "Category":
+                    switch (sortOrder)
+                    {
+                        case "Asc":
+                            urls = urls.OrderBy(x => x.tbl_Category.c_CategoryName).ToList();
+                            break;
+                        case "Desc":
+                            urls = urls.OrderByDescending(x => x.tbl_Category.c_CategoryName).ToList();
+                            break;
+                        default:
+                            break;
+                    }
+                    break;
+
+                default:
+                    urls = urls.OrderByDescending(x => x.c_UrlTitle).ToList();
+                    break;
+
+            }
+
+
             return View(urls);
         }
     }
