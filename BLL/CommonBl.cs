e5a5f68a4acab30496eb8d565ef34e09cc1665fa
nickgoodrow <nickgoodrow@gmail.com>
2015-10-16 20:04:29 -0400

    27: Transactions - TransactionScope and is Complete() method ROLLBACK functionality

--- BLL/CommonBl.cs
+++ BLL/CommonBl.cs
@@ -1,8 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Transactions;
 using BOL;
 
 namespace BLL
@@ -11,16 +8,30 @@ namespace BLL
     {
         public void InsertQuickSubmit(QuickSubmit quickSubmit)
         {
-            tbl_User user = quickSubmit.User;
-            user.c_Password = user.c_ConfirmPassword = new Random().Next(0, 100000).ToString("D6");
-            user.c_Role = User.RoleAdmin.Value;
-            userBl.Insert(user);
 
-            tbl_Url url = quickSubmit.Url;
-            url.i_UserId = user.i_UserId; // since inserted above it willl have an id generated for us by EF
-            url.c_UrlDesc = url.c_UrlTitle;
-            url.c_IsApproved = Url.StatusPending.Value;
-            urlBl.Insert(url);
+            using (TransactionScope transactionScope = new TransactionScope())
+            {
+                try
+                {
+                    tbl_User user = quickSubmit.User;
+                    user.c_Password = user.c_ConfirmPassword = new Random().Next(0, 100000).ToString("D6");
+                    user.c_Role = User.RoleAuthorizedUser.Value;
+                    userBl.Insert(user);
+
+                    tbl_Url url = quickSubmit.Url;
+                    url.i_UserId = user.i_UserId; // since inserted above it willl have an id generated for us by EF
+                    url.c_UrlDesc = url.c_UrlTitle;
+                    url.c_IsApproved = Url.StatusPending.Value;
+                    urlBl.Insert(url);
+
+                    // if does not rezch line below iot will rollback any ops/trans
+                    transactionScope.Complete();
+                }
+                catch (Exception e)
+                {
+                    throw new Exception(e.Message);
+                }
+            }
         }
     }
 }
